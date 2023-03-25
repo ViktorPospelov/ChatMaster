0b32e74da43c3824170570a0b0b2df00aab57196
Viktor <Vik.posp@gmail.com>
2023-03-25 14:30:45 +0500

    MainG

---
+++ ChatMaster/Assets/Script/Form/GameForm.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameForm : MonoBehaviour
+{
+    [SerializeField] private GameObject _formWinPopap;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
