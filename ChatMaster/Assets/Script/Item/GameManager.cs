1a2c46239f15974b4b697ea49f7614b451e76abf
Viktor <Vik.posp@gmail.com>
2023-04-13 18:51:59 +0500

    Update GameManager.cs

--- ChatMaster/Assets/Script/Item/GameManager.cs
+++ ChatMaster/Assets/Script/Item/GameManager.cs
@@ -9,17 +9,30 @@ public class GameManager
     private GameField _gameField;
     private LvL _lvL;
 
-    private int currentIndex = 0;
-    private int currentLvL;
+    private int _currentIndex = 0;
+    private int _currentLvL;
     public static Action<int, string> AnswerClick;
 
     public LvlState LvlState;
+
+    private Queue<Phrases> _toThePlayingField;
 
 
     public GameManager(LvL lvl, GameField gameField)
     {
         _gameField = gameField;
         _lvL = lvl;
-        currentLvL = _lvL.lvlNumber;
+        _currentLvL = _lvL.lvlNumber;
     }
+
+    private void MoveNext()
+    {
+        var ph =_toThePlayingField.Dequeue();
+    }
+    
+    private void AddPhrasesToQueue( Phrases phrases)
+    {
+        _toThePlayingField.Enqueue(phrases);
+    }
+    
 }
