89bb84bcd494f17af9dd5d5b12eb8bbd82476a18
Viktor <Vik.posp@gmail.com>
2023-04-15 15:02:23 +0500

    Update ItemCompanion.cs

--- ChatMaster/Assets/Script/Item/ItemCompanion.cs
+++ ChatMaster/Assets/Script/Item/ItemCompanion.cs
@@ -12,5 +12,42 @@ public class ItemCompanion : ItemBase
     
     [SerializeField] private GameObject _messageObject;
     
-   
+    [SerializeField] private Image _avatar;
+    [SerializeField] private GameObject _writes;
+    [SerializeField] private TextMeshProUGUI _message;
+    [SerializeField] private GameObject _messageObject;
+    
+    private const float _maxTimeWrite = 1.2f;
+    private const float _minTimeWrite = 0.6f;
+    
+
+    private IEnumerator StartAnimation(string message)
+    {
+        float timeWrite = _minTimeWrite;
+        timeWrite = Random.Range(_minTimeWrite,_maxTimeWrite) + (message.Length*_minTimeWrite/15f);
+        Debug.Log(timeWrite);
+        _messageObject.SetActive(false);
+        _writes.SetActive(true);
+        yield return new WaitForSeconds(timeWrite);
+        _writes.SetActive(false);
+        _messageObject.SetActive(true);
+        _message.text = message;
+        
+        GameField.NormalizePosition(this.gameObject);
+        StartCoroutine(MoveNext());
+        
+    }
+
+    private IEnumerator MoveNext()
+    {
+        yield return new WaitForSeconds(Random.Range(_minTimeWrite,_maxTimeWrite));
+        GameField.ConpanionEndWrite?.Invoke();
+    }
+    
+    
+
+    public void SetMessage(string masege)
+    {
+        StartCoroutine(StartAnimation(masege));
+    }
 }
